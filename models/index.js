110a05744fe806b58d424a059a143d5ed064142a
smudesign <urwinsamantha@gmail.com>
2021-11-11 17:01:31 -0500

    upvote posts

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// create associations
+// create one-to-many associations for posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,4 +11,34 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+// create many-to-many associations for votes
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// one-to-many for votes
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,19 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
 const { Module } = require('module');
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
+// get Posts
 router.get('/', (req, res) => {
     console.log('====================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -21,12 +29,19 @@ router.get('/', (req, res) => {
     });
 });
 
+// get Post by id
 router.get('/:id', (req, res) => {
     Post.findOne({
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -47,6 +62,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// create a Post
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
@@ -60,6 +76,17 @@ router.post('/', (req, res) => {
     });
 });
 
+// upvote a Post
+router.put('/upvote', (req, res) => {
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// update a Post's title
 router.put('/:id', (req, res) => {
     Post.update(
         {
@@ -84,6 +111,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete a Post
 router.delete('/:id', (req, res) => {
     Post.destroy({
         where: {
