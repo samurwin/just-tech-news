7a1533c9ee4ab8a0b48012e3828f9b7506910b3b
smudesign <urwinsamantha@gmail.com>
2021-11-17 13:51:56 -0500

    homepage rendering

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const routes = require('./routes');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,9 +12,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// make front-end static resources
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// set up handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
