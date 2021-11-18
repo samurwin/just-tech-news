6aeba50451b69d7f4a8650b9815aacf4d5a24303
smudesign <urwinsamantha@gmail.com>
2021-11-17 19:08:07 -0500

    log in and out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -56,13 +56,20 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users
 router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -87,8 +94,26 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect Password' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You are now logged in' });
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in' });
+        })
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
 });
 
 // update user
--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const hbs = exphbs.create({});
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
+require('dotenv').config();
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session(sess));
 
 // make front-end static resources
 app.use(express.static(path.join(__dirname, 'public')));
