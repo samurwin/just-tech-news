af77e73dba074b84aab01d18f5ac9a764d6e4f00
smudesign <urwinsamantha@gmail.com>
2021-11-17 20:31:24 -0500

    comments and upvotes when logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -94,12 +94,16 @@ router.post('/', (req, res) => {
 
 // upvote a Post
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 // update a Post's title
